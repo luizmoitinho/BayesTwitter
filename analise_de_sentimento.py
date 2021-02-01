0318c5089550c83a7fbd2ca328a3028feef19aac
eldeerick <erickoliveiraah@gmail.com>
2021-02-01 11:56:00 -0300

    modificacoes

--- analise_de_sentimento.py
+++ analise_de_sentimento.py
@@ -4,30 +4,19 @@ import numpy as np
 import time
 import os
 import re
-
-
 from IPython.display import display
 import matplotlib.pyplot as plt
 import seaborn as sns
 from wordcloud import WordCloud, ImageColorGenerator, STOPWORDS
-
-
-
 from textblob.classifiers import NaiveBayesClassifier
-
-
 import nltk
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
-
-
 from geopy.geocoders import Nominatim
 import folium
 from folium import plugins
-from geopy.geocoders import Nominatim
+#from geopy.geocoders import Nominatim
 #!python -m textblob.download_corpora
-
-
 
 #Keys for autentication
 consumer_key='srt5WEhYgrJ5SFGkCtkDSvjzH'
@@ -48,7 +37,7 @@ api = tweepy.API(auth)
 #Searchin Twitter Timelines
 tweets = []
 info = []
-
+count = 1
 for tweet in tweepy.Cursor(api.search,
                            q="assassino",
                            tweet_mode='extended',
@@ -66,11 +55,14 @@ for tweet in tweepy.Cursor(api.search,
     tweets.append(newtweet)
     info.append(tweet)
     
-    file = open("vacina.txt", "a", -1, "utf-8")
+    file = open("baseDados.txt", "a", -1, "utf-8")
     file.write(newtweet+'\n')
     file.close()
     
     time.sleep(0.5)
+    if(count == 5):
+        break
+    count = count + 1
 
 """### Creating the dataframe """
 
@@ -87,26 +79,10 @@ tweets_df['User Location']    = np.array([tweet.user.location for tweet in info]
 tweets_df['Geo']    = np.array([tweet.geo for tweet in info])
 tweets_df['Coordinates']    = np.array([tweet.coordinates for tweet in info])
 
-tweets_df.to_csv("vacina.csv")
+tweets_df.to_csv("baseDados.csv")
 
 tweets_df.head()
 
-"""### Top Tweets"""
-
-# Top Searched Tweets
-fav_max = np.max(tweets_df['Likes'])
-rt_max  = np.max(tweets_df['RTs'])
-
-fav = tweets_df[tweets_df.Likes == fav_max].index[0]
-rt  = tweets_df[tweets_df.RTs == rt_max].index[0]
-
-# Max FAVs:
-print("O tweet com mais curtidas é: \n{}".format(tweets_df['Tweets'][fav]))
-print("Número de curtidas: {}".format(fav_max))
-print("\n")
-# Max RTs:
-print("O tweet com mais retweet é: \n{}".format(tweets_df['Tweets'][rt]))
-print("Número de retweets: {}".format(rt_max))
 
 """### Source of Tweets"""
 
@@ -124,145 +100,65 @@ for source in tweets_df['Source']:
             percent[index] += 1
             pass
 
-newDF = pd.DataFrame({
- 'source':percent,
-}, index=sources)
-
-sources_sorted = newDF.sort_values('source',ascending=False)
-ax = sources_sorted.source.plot(kind='barh',color='#002060')
-ax.get_xaxis().set_major_formatter(plt.FuncFormatter(lambda x, loc: "{:,}".format(int(x))))
-
-"""### Training Model"""
-
-#Sentiment Analysis in Português and English
-
 base_path = 'ReLi-Lex'
 train = []
-wordsPT = []
-wordsPT_sentiments = []
+# wordsPT = []
+# wordsPT_sentiments = []
 
-files = [os.path.join(base_path, f) for f in os.listdir(base_path)]
+# files = [os.path.join(base_path, f) for f in os.listdir(base_path)]
 
-for file in files:
-    t = 1 if '_Positivos' in file else -1
-    with open(file, 'r', encoding='latin-1') as content_file:
-        content = content_file.read()
-        all = re.findall('\[.*?\]',content)
-        for w in all:
-            wordsPT.append((w[1:-1]))
-            wordsPT_sentiments.append(t)
-            train.append((w[1:-1], t))
+# for file in files:
+#     t = 1 if '_Positivos' in file else -1
+#     with open(file, 'r', encoding='latin1') as content_file:
+#         content = content_file.read()
+#         all = re.findall('\[.*?\]',content)
+#         for w in all:
+#             wordsPT.append((w[1:-1]))
+#             wordsPT_sentiments.append(t)
+#             train.append((w[1:-1], t))
 
-cl = NaiveBayesClassifier(train)
+#Importando o Léxico de Palavras com polaridades
+sentilexpt = open('SentiLex-lem-PT02.txt')
 
-"""### Word Cloud"""
+#Criando um dicionário de palavras com a respectiva polaridade.
 
-stopwords= set(STOPWORDS)
+dic_palavra_polaridade = []
+for i in sentilexpt.readlines():
+    pos_ponto = i.find('.')
+    palavra = (i[:pos_ponto])
+    pol_pos = i.find('POL')
+    polaridade = (i[pol_pos+7:pol_pos+9]).replace(';', '')
+    wordsPT.append(palavra)
+    wordsPT_sentiments.append(polaridade)
+    dic_palavra_polaridade.append((palavra, int(polaridade)))
 
-new_words = []
-with open("stopwords.txt", 'r') as f:
-    [new_words.append(word) for line in f for word in line.split()]
+'''
+========= COMENTARIOS =================
+(palavra, polaridade) * 7k =
 
-new_stopwords = stopwords.union(new_words)
+gente tinha mais  coisa la embaixo
 
-words = ' '.join(tweets_df['Tweets'])
+'''
 
-words_clean = " ".join([word for word in words.split()
-                            if 'https' not in word
-                                and not word.startswith('@')
-                                and word != 'RT'
-                            ])
-
-from imageio import imread
-import warnings
-
-warnings.simplefilter('ignore')
-
-twitter_mask = imread('download.png')
-
-wc = WordCloud(min_font_size=10, 
-               max_font_size=300, 
-               background_color='white', 
-               mode="RGB",
-               stopwords=new_stopwords,
-               width=2000, 
-               height=1000,
-               mask=twitter_mask,
-               normalize_plurals= True).generate(words_clean)
-
-plt.imshow(wc, interpolation="bilinear")
-plt.axis("off")
-plt.savefig('vacina.png', dpi=300)
-plt.show()
-
-"""### Time Series"""
-
-#Tweets per Day
-data = tweets_df
-
-data['Date'] = pd.to_datetime(data['Date']).apply(lambda x: x.date())
-
-tlen = pd.Series(data['Date'].value_counts(), index=data['Date'])
-tlen.plot(figsize=(16,4), color='r');
-
-"""# Twitter Setiments Analisys with NLTK
-
-### Training Model
-"""
-
-vectorizer = CountVectorizer(analyzer="word")
-freq_tweets = vectorizer.fit_transform(wordsPT)
-modelo = MultinomialNB()
-modelo.fit(freq_tweets,wordsPT_sentiments);
-
-"""### Sentiment Analisys"""
+cl = NaiveBayesClassifier(dic_palavra_polaridade)
 
 def clean_tweet(tweet):
     return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
 
 tweetsarray = []
-
 for tw in tweets_df['Tweets']:
     text = clean_tweet(tw)
-    tweetsarray.append(text)
+    tweetsarray.append(tweet)
 
 predictionData = vectorizer.transform(tweets_df['Tweets'])
 tweets_df['SA NLTK'] = modelo.predict(predictionData)
 
-"""### Results of Sentiment Analysis"""
 
 #Sentiment Analysis Results
 pos_tweets = [ tweet for index, tweet in enumerate(tweets_df['Tweets']) if tweets_df['SA NLTK'][index] > 0]
 neg_tweets = [ tweet for index, tweet in enumerate(tweets_df['Tweets']) if tweets_df['SA NLTK'][index] < 0]
 
+
 print("Porcentagem de Tweets Positivos: {}%".format(len(pos_tweets)*100/len(tweets_df['Tweets'])))
 print("Porcentagem de Tweets Negativos: {}%".format(len(neg_tweets)*100/len(tweets_df['Tweets'])))
 
-sentiments = ['Positivos', 'Negativos']
-percents = [len(pos_tweets), len(neg_tweets)]
-
-pie_chart = pd.Series(percents, index=sentiments, name='Sentimentos')
-pie_chart.plot.pie(fontsize=11, autopct='%.2f', figsize=(6, 6));
-
-"""### Map of Tweets"""
-
-geolocator = Nominatim(user_agent="TweeterSentiments")
-
-latitude = []
-longitude = []
-
-for user_location in tweets_df['User Location']:
-    try:
-        location = geolocator.geocode(user_location)
-        latitude.append(location.latitude)
-        longitude.append(location.longitude)
-    except:
-        continue
-
-coordenadas = np.column_stack((latitude, longitude))
-
-mapa = folium.Map(location=[-15.788497,-47.879873],zoom_start=4.)
-
-mapa.add_child(plugins.HeatMap(coordenadas))
-mapa.save('vacina.html')
-mapa
